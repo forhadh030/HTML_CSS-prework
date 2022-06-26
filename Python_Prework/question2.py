5c9b8db3233766e3fa61cd8e7642bae28335b059
Forhad Hussain <forhad@ymail.com>
2022-06-25 21:55:24 -0700

    update to the python prework question

--- Python_Prework/question2.py
+++
@@ -1,9 +0,0 @@
-# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
-
-def first_odds():
-    first_odds = 0
-    for i in range(1, 100, 2):
-        odd_numbers = i + first_odds
-        print(odd_numbers)
-
-first_odds()
--- Python_Prework/question3.py
+++
@@ -1,8 +0,0 @@
-# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
-max_list = [50, 10, 100, 1, 25]
-def max_num_in_list(a_list):
-    return max_list
-
-print(max(max_list))
-print(len(max_list))
-print(sum(max_list))
